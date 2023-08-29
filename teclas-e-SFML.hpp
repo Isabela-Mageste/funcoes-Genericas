04bc485286fe3ba02052c776b195d939ade80fa9
Mageste <115092439+Itsukki-25@users.noreply.github.com>
2023-08-29 19:56:03 -0300

    Create teclas-e-SFML.hpp
    como trabalhar com teclas em sfml :)

---
+++ teclas-e-SFML.hpp
@@ -0,0 +1,47 @@
+if (event.type == sf::Event::KeyPressed) {
+//					if (event.key.code == sf::Keyboard::Left) {
+//						velx = -1;
+//						//view.move(-10, 00);
+//
+//					} else if (event.key.code == sf::Keyboard::Right) {
+//						velx = 1;
+//						//view.move(10, 00);
+//
+//					} else if (event.key.code == sf::Keyboard::Down) {
+//						vely = 1;
+//						//view.move(00, 10);
+//
+//					} else if (event.key.code == sf::Keyboard::Up) {
+//						vely = -1;
+//						//view.move(00, -10);
+//
+//					}
+//
+//				}
+//
+//				if (event.type == sf::Event::KeyReleased) {
+//					if (event.key.code == sf::Keyboard::Left) {
+//						velx = 0;
+//						//view.move(-10, 00);
+//
+//					} else if (event.key.code == sf::Keyboard::Right) {
+//						velx = 0;
+//						//view.move(10, 00);
+//
+//					} else if (event.key.code == sf::Keyboard::Down) {
+//						vely = 0;
+//						//view.move(00, 10);
+//
+//					} else if (event.key.code == sf::Keyboard::Up) {
+//						vely = 0;
+//						//view.move(00, -10);
+//
+//					}
+//
+//				}
+
+/*
+isso pega a tecla pressionada e move a visao da tela, mas pode ser adaptado pra qualer coisa que use teclas :)
+
+
+*/
